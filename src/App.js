05b4603e54800567f87797580ddbc4e8b57af471
Tanner Scadden <tanner@scaddenfamily.com>
2019-03-26 13:34:45 -0600

    finished project and black diamond

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { HashRouter, Link } from 'react-router-dom';
+
+import routes from './routes';
 
 export default class App extends Component {
   render() {
     return (
+      <HashRouter>
       <div>
         <nav className='nav'>
-          <div>WestSide University</div> 
+        <Link to="/"><div>WestSide University</div></Link>
           <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
+              <Link to="/"><div className="links">Home</div></Link>
+              <Link to="/about"><div className='links'>About</div> </Link>
           </div>
         </nav>
+        {routes}
       </div>
-    )
-  }
-}
+      </HashRouter>
+    );
+  };
+};
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super()
     
-  }
+    this.state = {
+      students: []
+    };
+  };
+
+  componentDidMount() {
+    Axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(res => {
+      this.setState({ students: res.data });
+    }).catch(err => console.log("We have an error", err));
+  };
 
   render() {
+    let student = this.state.students.map((student, index) => {
+      return(
+        <Link to={`/student/${student.id}`} key={index}>
+          <h3>{student.first_name} {student.last_name}</h3>
+        </Link>
+      )
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {student}
+        <Link to="/"><button>Go Back</button></Link>
       </div>
-    )
-  }
-}
+    );
+  };
+};
